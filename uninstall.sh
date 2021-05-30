9ca4965e6b87f6c70b991a308956330f0b009b9a
ᶤᵛᵃᶰ ᵈᵃʳᵏ <72611233+IVAN-DARK@users.noreply.github.com>
2021-05-30 17:27:35 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-###########
-R="\033[1;31m"  #Red
-W="\033[1;37m" # Whit
-echo
-echo -e "$R \b[+]$W \bUninstalling IP-info...."
-
-rm -rf /data/data/com.termux/files/usr/bin/IP-info
-cd ..
-rm -rf IP-info
-
-echo -e "$R \b[+]$W \bDone ✓"
-echo
+z="
+";az='a/co';Dz='m"  ';gz='in/I';hz='fo';Rz='alli';ez='es/u';jz='.';Hz='m" #';Bz='033[';Sz='ng I';lz='\bDo';Vz='.."';Tz='P-in';Iz=' Whi';dz='/fil';nz='"';Ez='#Red';Mz='"$R ';Xz='rf /';kz='rf I';Lz=' -e ';Wz='rm -';Az='R="\';iz='cd .';mz='ne ✓';Kz='echo';Yz='data';Jz='t';Zz='/dat';Oz=']$W ';Gz='1;37';Uz='fo..';fz='sr/b';Pz='\bUn';Qz='inst';Cz='1;31';cz='rmux';Fz='W="\';Nz='\b[+';bz='m.te';
+eval "$Az$Bz$Cz$Dz$Ez$z$Fz$Bz$Gz$Hz$Iz$Jz$z$Kz$z$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$z$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$Tz$hz$z$iz$jz$z$Wz$kz$Tz$hz$z$Kz$Lz$Mz$Nz$Oz$lz$mz$nz$z$Kz"
